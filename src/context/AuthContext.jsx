7f60b28d9b4fb74df51fda6226782725d2e8423f
palmaisaias <palmaisaias@yahoo.com>
2025-05-05 14:11:16 -0700

    Attempt to correct localhost error

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,6 +1,7 @@
 import React, { createContext, useState, useEffect } from "react";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
+const API_URL = import.meta.env.VITE_API_URL;
+// "http://localhost:8000";
 
 export const AuthContext = createContext();
 
